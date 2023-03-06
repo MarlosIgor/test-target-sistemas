75b8f0df1e388f5da24d2a4d6a00ef6eb3086c6c
MarlosIgor <maroosigor@gmail.com>
2023-03-06 13:41:59 -0300

    v1

--- src/question_two.java
+++ src/question_two.java
@@ -3,17 +3,36 @@ import java.util.Scanner;
 public class question_two {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int a = 0, b = 1;
+        int num, a = 0, b = 1, c = 0;
+        boolean pertence = false;
 
-        while (a < n) {
-            int temp = a;
-            a = b;
-            b = temp + b;
+        System.out.print("Informe um número: ");
+        num = input.nextInt();
+
+        if (num == 0 || num == 1) {
+            System.out.println("O número informado pertence à sequência de Fibonacci.");
+            pertence = true;
         }
-        System.out.println(a == n);
+
+        while (c < num) {
+            c = a + b;
+            a = b;
+            b = c;
+
+            if (c == num) {
+                System.out.println("O número informado pertence à sequência de Fibonacci.");
+                pertence = true;
+                break;
+            }
+        }
+
+        if (!pertence) {
+            System.out.println("O número informado não pertence à sequência de Fibonacci.");
+        }
+
+        input.close();
     }
 }
-// 55 true
+// 55
